4f20a30bc0a40682dc8adcd2060cd1ef3bfabec6
abdullahkhalidmahmood <100064352@students.swinburne.edu.my>
2020-05-28 01:56:08 +0800

    Constants class added

---
+++ app/src/main/java/com/example/week7demo/Constants.java
@@ -0,0 +1,10 @@
+package com.example.week7demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Constants {
+    public static ArrayList<GameModel> fpsList=new ArrayList<>();
+    public static ArrayList<GameModel> rpList=new ArrayList<>();
+
+}
